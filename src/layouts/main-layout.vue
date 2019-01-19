0b1850bf2c56ebcbcfc46f46ef3113abcac56731
lektra <annvolkovych@gmail.com>
2019-01-19 17:35:47 +0200

    Initial commit

---
+++ src/layouts/main-layout.vue
@@ -0,0 +1,20 @@
+<template>
+    <main>
+        <main-header></main-header>
+        <slot></slot>
+        <main-footer></main-footer>
+    </main>
+</template>
+
+<script>
+import MainHeader from '@/components/main-header'
+import MainFooter from '@/components/main-footer'
+
+export default {
+    name: 'MainLayout',
+    components: {
+        MainHeader,
+        MainFooter
+    }
+}
+</script>
